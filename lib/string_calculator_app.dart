2d40c2d5edb566ebbf15be909db8ce23efb31ab4
sureshksharma <sureshksharma51100@gmail.com>
2025-06-28 15:42:34 +0530

    Added custom delimiter case

--- lib/string_calculator_app.dart
+++ lib/string_calculator_app.dart
@@ -1,5 +1,17 @@
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
-  final parts = numbers.split(RegExp('[,\n]'));
+  String delimiterPattern = '[,\n]';
+  if (numbers.startsWith('//')) {
+    final parts = numbers.split('\n');
+    final delimeterLine = parts[0];
+    final delimeter = RegExp.escape(delimeterLine.substring(2));
+    print('delimeter: $delimeter');
+    delimiterPattern = '[$delimeter\n]';
+    print('delimiterPattern: $delimiterPattern');
+    print('sublist: ${parts.sublist(1)}');
+    numbers = parts.sublist(1).join('\n');
+    print('numbers: $numbers');
+  }
+  final parts = numbers.split(RegExp(delimiterPattern));
   return parts.map(int.parse).reduce((a, b) => a + b);
 }
--- test/string_calculator_app_test.dart
+++ test/string_calculator_app_test.dart
@@ -18,7 +18,15 @@ void main() {
     expect(add('1,5,3,2'), 11);
   });
 
-  test('Support new line as delimiter', () {
+  test('New line as delimiter', () {
     expect(add('1\n2,3'), 6);
+  });
+
+  test('Custom delimiter in numbers', () {
+    expect(add('//;\n1;2'), 3);
+  });
+
+  test('Custom delimiter in numbers', () {
+    expect(add('//\$\n1\$2'), 3);
   });
 }
